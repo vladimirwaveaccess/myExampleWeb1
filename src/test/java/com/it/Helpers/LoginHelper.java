803f6c541535700b9e965ab668668d0c2bcc84d4
vladimirwaveaccess <vladimir.agafonov2011@gmail.com>
2018-06-19 21:20:40 +0300

    commit Logger

--- src/test/java/com/it/Helpers/LoginHelper.java
+++ src/test/java/com/it/Helpers/LoginHelper.java
@@ -2,10 +2,12 @@ package com.it.Helpers;
 
 import com.it.Pages.LoginPage;
 import com.it.Utils.Users.User;
+import org.apache.log4j.Logger;
 
 public class LoginHelper extends LoginPage {
 
     public void login(User user) {
+        log.info(String.format("User login: userName - %s, password -%s", user.getLogin(), user.getPassword()));
         login(user.getLogin(), user.getPassword());
     }
 }
--- src/test/java/com/it/Pages/BasePage.java
+++ src/test/java/com/it/Pages/BasePage.java
@@ -2,13 +2,15 @@ package com.it.Pages;
 
 import com.it.Common.Constants;
 import com.it.Drivers.DriverFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class BasePage {
-
+    protected Logger log = LogManager.getLogger(this.getClass());
     protected static WebDriver driver = DriverFactory.getDriver();
 
     public BasePage() {
